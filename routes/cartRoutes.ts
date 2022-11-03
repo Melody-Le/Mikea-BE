155bd9018c5bdff298655f1da6ac16d7d5925e4d
MUI LE <tanmui1992@gmail.com>
2022-11-03 19:27:48 +0800

    feat: remove findOrcrate cart route

--- routes/cartRoutes.ts
+++ routes/cartRoutes.ts
@@ -11,7 +11,7 @@ import {
 const router = Router();
 
 router.get("/", authMiddleware, showCart);
-router.post("/create", authMiddleware, findOrCreateCart);
+// router.post("/create", authMiddleware, findOrCreateCart);
 //
 router.post("/add/:variantId", authMiddleware, addItemToCart);
 
